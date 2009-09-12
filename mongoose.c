fe3abbc7076b0c62b4cc346b809d60d83925effc
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-09-12 15:30:01 +0200

    mongoose.c: Update copyright information.

--- mongoose.c
+++ mongoose.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2004-2009 Sergey Lyubka
  * Portions Copyright (c) 2009 Gilbert Wellisch
+ * Copyright (c) 2009 Florian Forster
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
